683b821dac194ae9cabe36c45aab414beedc72a2
asimonweb <asimonwebdev@gmail.com>
2018-06-16 14:32:14 +0200

    Ticket entity and database migration

---
+++ src/Entity/Ticket.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\TicketRepository")
+ */
+class Ticket
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $bookingRef;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $pricing;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $firstName;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $lastName;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $country;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $birthDate;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getBookingRef(): ?string
+    {
+        return $this->bookingRef;
+    }
+
+    public function setBookingRef(string $bookingRef): self
+    {
+        $this->bookingRef = $bookingRef;
+
+        return $this;
+    }
+
+    public function getPricing(): ?string
+    {
+        return $this->pricing;
+    }
+
+    public function setPricing(string $pricing): self
+    {
+        $this->pricing = $pricing;
+
+        return $this;
+    }
+
+    public function getFirstName(): ?string
+    {
+        return $this->firstName;
+    }
+
+    public function setFirstName(string $firstName): self
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    public function getLastName(): ?string
+    {
+        return $this->lastName;
+    }
+
+    public function setLastName(string $lastName): self
+    {
+        $this->lastName = $lastName;
+
+        return $this;
+    }
+
+    public function getCountry(): ?string
+    {
+        return $this->country;
+    }
+
+    public function setCountry(string $country): self
+    {
+        $this->country = $country;
+
+        return $this;
+    }
+
+    public function getBirthDate(): ?\DateTimeInterface
+    {
+        return $this->birthDate;
+    }
+
+    public function setBirthDate(\DateTimeInterface $birthDate): self
+    {
+        $this->birthDate = $birthDate;
+
+        return $this;
+    }
+}
